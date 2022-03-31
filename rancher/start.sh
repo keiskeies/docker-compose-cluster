f65b61b2f613b2942143f3d8303972604acf2d37
keiskeis <right_way@foxmail.com>
2022-03-31 14:55:24 +0800

    l

--- rancher/start.sh
+++ rancher/start.sh
@@ -6,7 +6,7 @@ docker network rm rancher_network
 
 docker network create rancher_network --subnet 172.21.0.0/16
 
-docker run -d --name=rancher_1 --net rancher_network --ip 172.21.0.2 --restart=unless-stopped -p 28081:8080 -p 29346:9345 -p 23307:3306 -v /var/lib/docker/volumes/rancher/mysql/:/var/lib/mysql rancher/server:stable --advertise-address 192.168.31.236 --advertise-http-port 28081
+docker run -d --name=rancher_1 --net rancher_network --ip 172.21.0.2 --restart=unless-stopped -p 8080:8080 -p 9345:9345 -p 23307:3306 -v /var/lib/docker/volumes/rancher/mysql/:/var/lib/mysql rancher/server:stable --advertise-address 192.168.31.236 --advertise-http-port 8080
 
 
 docker ps -a
---
+++ registry/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+docker rm -f registry_1 registry_2 registry_3 registry_ui
+docker-compose up -d
